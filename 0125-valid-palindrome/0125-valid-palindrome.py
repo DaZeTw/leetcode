3b8170f388b1dda01f0e228a5ede05dcc930baa8
Dalton Pham <92284124+DaZeTw@users.noreply.github.com>
2025-05-27 22:24:20 +0700

    Time: 8 ms (52.84%), Space: 23.7 MB (5.23%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,20 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        new_s=""
-        # new_s=''.join(c for c in s if c.isalpha())
-        new_s=''.join(c for c in s if c.isalnum())
-        new_s=new_s.lower()
-        # print(new_s)
-        # n = len(new_s)
-        # for i in range(0,n//2):
-        #     print(i)
-        #     if (new_s[i]!=new_s[n-i-1]): return False
-        # return True
-        if(new_s==new_s[::-1]): 
-            return True
-        else:
-            return False
-
+        filtered_chars = [c.lower() for c in s if c.isalnum()]
         
-
-        
+        # Check if the filtered characters form a palindrome
+        return filtered_chars == filtered_chars[::-1]
